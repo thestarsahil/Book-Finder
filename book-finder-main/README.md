bedc0182cb65a4b77ccde164db63d6925efb2065
Sahil Ali <84167034+thestarsahil@users.noreply.github.com>
2023-04-24 10:26:07 +0530

    Add files via upload

---
+++ book-finder-main/README.md
@@ -0,0 +1,119 @@
+<p align="center">
+<img src="https://tipseason.com/assets/images/hacktoberfest-banner.jpg" alt="hacktoberfest-banner" width="640" height="320" />
+<br><br>
+<img src="https://d33wubrfki0l68.cloudfront.net/710cb0d537d2e7fa6246445c0977ecc994f05f5a/6a3b4/assets/img/jwoc_logo.svg" alt="jwoc" width="300" />
+</p>
+<h1 align="center">Book Finder</h1>
+<h3 align="center">Find the book you want to read next!</h3>
+<div align="center">
+<img src="https://forthebadge.com/images/badges/built-with-love.svg" />
+<img src="https://forthebadge.com/images/badges/powered-by-coffee.svg" />
+<img src="https://forthebadge.com/images/badges/open-source.svg" />
+</div>
+
+## About The Project
+
+Book Finder is your go-to place for all kinds of book related searches, need to read a book but don't know where to find it?
+
+We got your back! Started in 2022, we are an open-source free search engine with over a million books and more to find out.
+Here you won't just be able to read books but also listen to them.
+
+So what are you waiting for? Search your favourite book right now!
+
+## Features
+
+- Allows user to search books by title, author and ISBN number.
+- Allows user to read the preview of the book.
+- Generate picture of book along with external link to see a preview of the book on Google Books.
+- Along with the books for reading, audiobooks are also available.
+
+## Tech Stack
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) 
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)
+![Bootstrap](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)
+
+## Getting Started
+
+Below are the steps to follow to contribute to this project:
+
+**1.** Fork [this](https://github.com/wasimreja/book-finder) repository.
+
+**2.** Clone your forked copy of the project.
+
+```
+git clone https://github.com/<your_user_name>/book-finder.git
+```
+
+where `your_user_name` is your GitHub username.
+
+**3.** Navigate to the project directory.
+
+```
+cd book-finder
+```
+
+**4.** Add a reference(remote) to the original repository.
+
+```
+git remote add upstream https://github.com/wasimreja/book-finder.git
+```
+
+**5.** Check the remotes for this repository.
+
+```
+git remote -v
+```
+
+**6.** Always take a pull from the upstream repository to your main branch to keep it at par with the main project(updated repository). Feel free to raise new issues.
+
+```
+git pull upstream main
+```
+
+**7.** Create a new branch.
+
+```
+git checkout -b <your_branch_name>
+```
+
+**8.** Make necessary changes and commit those changes
+
+**9.** Track your changes.
+
+```
+git add .
+```
+
+**10.** Commit your changes .
+
+```
+git commit -m "bla bla bla"
+```
+
+**11.** Push the committed changes in your feature branch to your remote repo.
+
+```
+git push -u origin <your_branch_name>
+```
+
+**12.** To create a pull request, click on `Compare & pull request`. Please ensure you compare your feature branch to the desired branch of the repo you are suppose to make a PR to.
+
+**13.** Add appropriate title and description to your pull request explaining your changes and efforts done.
+
+**14.** Click on `Create pull request`.
+
+**15.** And you are done creating a pull request to this project. Be patient while your PR is reviewed.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contributors
+
+Thanks to these wonderful people ✨
+
+<a href="https://github.com/wasimreja/book-finder/graphs/contributors">
+<img src="https://contrib.rocks/image?repo=wasimreja/book-finder" />
+</a>
---
+++ book-finder-main/js/main.js
@@ -0,0 +1,259 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener(" click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+function listen(e, pause) {
+  const bookCard = e.target.parentElement;
+  const bookDescription = bookCard.children[2].innerText;
+  const bookName = bookCard.children[0].innerText;
+  const authorName = bookCard.children[1].innerText;
+
+  const message = `The name of the book is ${bookName}  . It is written ${authorName} .  ${bookDescription}`;
+
+  let synth = speechSynthesis;
+  synth.cancel();
+  if (pause) {
+    synth.pause();
+  }
+  setTimeout(() => {
+    const speech = new SpeechSynthesisUtterance(message);
+    speech.lang = "en-US";
+    synth.speak(speech);
+  }, 1000);
+}
+
+let currPage =1;
+
+function search(e) {
+  e.preventDefault();
+  let search = document.getElementById("input").value;
+  setTimeout(function() {$('#pagination-wrapper').removeClass('hidden');}, 2000);
+  if (search.trim() === "") return;
+  document.activeElement.blur(); // this removes focus on the input bar after search
+  initiateApi(currPage,search);
+}
+
+function nextPage() {
+  let search = document.getElementById("input").value;
+  currPage++;
+  initiateApi(currPage,search);
+}
+
+
+function prevPage() {
+  let search = document.getElementById("input").value;
+  currPage--;
+  initiateApi(currPage,search);
+}
+
+
+  function initiateApi(curr,search){
+    console.log(`https://www.googleapis.com/books/v1/volumes?q="${search}"&maxResults=5&startIndex=`+(curr-1)*5);
+
+  $.ajax({
+    url: `https://www.googleapis.com/books/v1/volumes?q="${search}"&maxResults=5&startIndex=`+(curr-1)*5
+    ,
+    dataType: "json",
+    beforeSend: function () {
+      $(".whirly-loader").show();
+    },
+    complete: function () {
+      $(".whirly-loader").hide();
+    },
+
+    success: function (res) {
+      const resultsContainer = document.getElementById("results");
+      while (resultsContainer.firstChild) {
+        resultsContainer.removeChild(resultsContainer.firstChild);
+      }
+
+      let bookNotFound = res.totalItems === 0;
+
+      if (bookNotFound) {
+
+        // let notfound = document.createElement("DIV");
+        // notfound.innerHTML = `
+        // <div class="d-flex flex-column flex-sm-row align-items-center justify-content-center text-center text-sm-left error-message"
+        //      color=(icon.classList.contains('fa-moon') ? 'text-white' : 'text-dark'>
+        //     <img src="./img/file-not-found.gif" alt="404 error" width="100" height="100" class="m-2">
+        //     <div>
+        //       <p class="lead"> <span class="text-danger">Oops!</span> Book not found.</p>
+        //       <p class="fs-3">The book you’re looking for doesn’t exist.</p>
+        //     </div>
+        // </div>
+        // `;
+
+        // document.getElementById("results").appendChild(notfound);
+        
+        location.replace("404.html"); // Redirect to 404 page
+      } else {
+        for (let i = 0; i < res.items.length; i++) {
+          // DIV
+          const bookCard = document.createElement("div");
+
+          // Image
+          if (res.items[i].volumeInfo.imageLinks) {
+            var bookImageContainer = document.createElement("div");
+            bookImageContainer.classList.add("col-md-2", "offset-md-2");
+
+            const bookImage = document.createElement("img");
+            bookImage.src = res.items[i].volumeInfo.imageLinks.smallThumbnail;
+            bookImage.classList.add("w-100");
+
+            bookImageContainer.appendChild(bookImage);
+          }
+
+          // Title
+          const bookInfo = document.createElement("div");
+          bookInfo.classList.add("col-md-8");
+          const bookTitle = document.createElement("h1");
+          bookTitle.textContent = res.items[i].volumeInfo.title;
+
+          //Author
+          if (res.items[i].volumeInfo.authors) {
+            var bookAuthor = document.createElement("h6");
+            bookAuthor.textContent = `by ${
+              res.items[i].volumeInfo.authors[0]
+                ? res.items[i].volumeInfo.authors[0]
+                : "No title"
+            }`;
+          }
+
+          // Description
+          const bookDescription = document.createElement("p");
+          bookDescription.classList.add("description");
+          const desc = res.items[i].volumeInfo.description
+            ? res.items[i].volumeInfo.description
+            : "No description";
+
+          const shortPar = document.createElement("span");
+          shortPar.classList.add("short-description");
+          const shortDesc = document.createTextNode(desc.substring(0, 100));
+          shortPar.appendChild(shortDesc);
+
+          const remainingPar = document.createElement("span");
+          remainingPar.classList.add("remaining-description");
+          const remainingDesc = document.createTextNode(desc.substring(100));
+          remainingPar.appendChild(remainingDesc);
+
+          const readMoreBtn = document.createElement("span");
+          readMoreBtn.classList.add("read-more-btn");
+          const readMoreBtnText = document.createTextNode(" ...Read More");
+          readMoreBtn.appendChild(readMoreBtnText);
+          readMoreBtn.addEventListener("click", (e) => {
+            const remainingDescription = e.target.parentNode.querySelector(
+              ".remaining-description"
+            );
+            remainingDescription.classList.toggle(
+              "remaining-description--show"
+            );
+
+            e.target.textContent = e.target.textContent.includes(
+              " ...Read More"
+            )
+              ? " ...Read Less"
+              : " ...Read More";
+          });
+
+          bookDescription.appendChild(shortPar);
+          bookDescription.appendChild(remainingPar);
+          if (desc !== "No description") {
+            bookDescription.appendChild(readMoreBtn);
+          }
+
+          // Button
+          const bookPreviewLink = document.createElement("a");
+          bookPreviewLink.innerHTML = "READ";
+          bookPreviewLink.href = res.items[i].volumeInfo.previewLink;
+          bookPreviewLink.target = "blank";
+
+          const speechButton = document.createElement("button");
+          speechButton.classList.add("listen", "btn", "btn-outline-secondary");
+          speechButton.textContent = "LISTEN";
+
+          bookPreviewLink.classList.add("btn", "btn-outline-secondary");
+          bookCard.classList.add("result", "row");
+          bookCard.setAttribute("data-aos", "fade-up");
+
+          bookInfo.append(
+            bookTitle,
+            bookAuthor,
+            bookDescription,
+            bookPreviewLink,
+            speechButton
+          );
+
+          bookCard.append(bookInfo, bookImageContainer);
+          document.getElementById("results").appendChild(bookCard);
+          document.getElementById("results").scrollIntoView();
+        }
+        const speechButtons = document.querySelectorAll(".listen");
+        for (const speechButton of speechButtons) {
+          let isOn = false;
+          speechButton.addEventListener("click", (e) => {
+            console.log(e);
+            console.log(speechButton.textContent);
+            if (speechButton.textContent == "LISTEN") {
+              speechButton.textContent = "STOP";
+            } else {
+              speechButton.textContent = "LISTEN";
+            }
+            isOn = !isOn;
+            if (isOn) {
+              listen(e, false);
+            } else {
+              listen(e, true);
+            }
+          });
+        }
+      }
+    },
+    maxResults: 30,
+    type: "GET",
+  });
+}
+
+document.querySelector(".search-form").addEventListener("submit", search);
+
+const scroll = document.getElementById("return-to-top");
+window.onscroll = () => scrollFunction();
+function scrollFunction() {
+  if (document.body.scrollTop || document.documentElement.scrollTop > 20) {
+    scroll.classList.remove("scroll-container");
+  } else {
+    scroll.classList.add("scroll-container");
+  }
+}
+
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "img/sun.png";
+    localStorage.setItem("theme", "dark");
+  } else {
+    icon.src = "img/moon.png";
+    localStorage.setItem("theme", "light");
+  }
+};
+
+const initIcon = () => {
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "img/sun.png";
+  } else {
+    icon.src = "img/moon.png";
+  }
+};
+window.onload = initIcon();
+
+
+
+
